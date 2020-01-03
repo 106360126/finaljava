d82975a4070a432af5faf63fa47ccfca9a115e1f
106360126 <t106360126@ntut.org.tw>
2020-01-03 16:19:55 +0800

    Main1

--- app/src/main/java/com/example/finaljava/MainActivity.java
+++ app/src/main/java/com/example/finaljava/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finaljava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.taichung).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String City = "taichung";
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("City",City);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.taipei).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String City = "taipei";
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("City",City);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.tainan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String City = "tainan";
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("City",City);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.pingtung).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String City = "pingtung";
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("City",City);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.kaohsiung).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String City = "kaohsiung";
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("City",City);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.taitung).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String City = "taitung";
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("City",City);
+                startActivity(intent);
+            }
+        });
     }
 }
